c064e4c457ca36ef9b0508b764decf3b6634e8d1
OSX laptop <oskarek.2005.ok@gmail.com>
2024-11-23 18:00:19 +0100

    finally fixed set_map_for_drone and Map logic

--- nodes/Map.py
+++ nodes/Map.py
@@ -6,8 +6,8 @@ class Map:
         self.bottom_left = None
 
     def set_corners(self):
-        self.top_right = (self.bottom_right[0], self.top_left[1])
-        self.bottom_left = (self.top_left[0], self.bottom_right[1])
+        self.bottom_left = (self.bottom_right[0], self.top_left[1])
+        self.top_right = (self.top_left[0], self.bottom_right[1])
 
     def get_top_left(self):
         return self.top_left
--- nodes/utils.py
+++ nodes/utils.py
@@ -2,13 +2,13 @@ from Map import Map
 import math
 def set_map_for_drone(drone_map: Map, drones: list):
     number_of_drones = len(drones)
-    width = drone_map.top_left[0] - drone_map.top_right[0]
-    drone_field_width = width // number_of_drones
+    width = abs(drone_map.top_left[1] - drone_map.top_right[1])
+    drone_field_width = width / number_of_drones
     for i, drone in enumerate(drones):
-        drone_top_left = drone_map.top_left[0] + i * drone_field_width
-        drone_bottom_right = drone_map.bottom_left[0] + i * drone_field_width
-        drone_map = Map(drone_top_left, drone_bottom_right)
-        drone.setMap(drone_map)
+        drone_top_left = drone_map.top_left[0], drone_map.top_left[1] + i*drone_field_width
+        drone_bottom_right = drone_map.bottom_right[0], drone_map.top_left[1] +(i+1)*drone_field_width
+        drone_map_new = Map(drone_top_left, drone_bottom_right)
+        drone.setMap(drone_map_new)
 
 def haversine(lat1, lon1, lat2, lon2):
 
