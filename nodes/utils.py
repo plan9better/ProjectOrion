346a8cefda5cbd20daffad148c3a4c184ec3c490
stanidzior <stanosik123@gmail.com>
2024-11-23 13:14:14 +0100

    poprawiam poprzedniego

--- nodes/utils.py
+++ nodes/utils.py
@@ -23,16 +23,14 @@ def haversine(lat1, lon1, lat2, lon2):
 
     dlat = lat2_rad - lat1_rad
     dlon = lon2_rad - lon1_rad
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    distance = R * c
+    return distance
 
 def get_area_fit_count(area1, area2):
     if (area1 < area2):
         return
     fitCount = math.ceil(area1 / area2)
     return fitCount
-
-
-    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    distance = R * c
-    return distance
