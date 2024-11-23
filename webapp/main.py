47a95822bbd557749740dc47d13c41ae4cfee252
Sewimi <dawidf0103@gmail.com>
2024-11-23 04:10:48 -0500

    minor changes

--- webapp/main.py
+++ webapp/main.py
@@ -1,60 +1,35 @@
 from flask import Flask, render_template, request, jsonify
 import osmnx as ox
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+# hive_controller = new HiveController()
+
+
+def start_mission():
+    #algos.get_drones_data()
+    #hive_controller.start_mission()
+    pass
+
 
 @app.route('/')
 def index():
-    center_point = (54.352014, 18.648392)
-    radius = 1000
+    pass
 
 
-    G = ox.graph_from_point(center_point, dist=radius, network_type='walk')
+@app.route("/mission_data")
+def get_mission_data():
+    pass
 
-    # Extract nodes and edges from the graph as GeoDataFrames
-    nodes, edges = ox.graph_to_gdfs(G)
+@socketio.on('broadcast')
+def update_map():
+    emit({"drones"})
 
-    # Convert edge geometries to GeoJSON
-    edge_geojson = edges.to_json()
+def send_drones():
+    pass
 
-    # Convert node geometries to GeoJSON
-    node_geojson = nodes.to_json()
-
-    return render_template('trash.html', center=center_point, edge_geojson=edge_geojson, node_geojson=node_geojson)
-
-
-@app.route('/select-point', methods=['POST'])
-def select_point():
-    # Get the data sent from the client
-    data = request.get_json()
-
-    # Extract latitude and longitude
-    lat = data.get('lat')
-    lng = data.get('lng')
-
-    print(f"Received point: Latitude = {lat}, Longitude = {lng}")
-
-    # You can process or save the data as needed here
-    # For example, store the coordinates in a database
-
-    # Respond to the client
-    return jsonify({"message": "Point received successfully!", "lat": lat, "lng": lng})
-
-@app.route('/draw-rectangle', methods=['POST'])
-def draw_rectangle():
-    # Get the rectangle data sent from the client
-    data = request.get_json()
-
-    # Extract top-left and bottom-right coordinates
-    top_left = data.get('topLeft')
-    bottom_right = data.get('bottomRight')
-
-    print(f"Rectangle received: Top-Left = {top_left}, Bottom-Right = {bottom_right}")
-
-    # Process or store the coordinates (e.g., in a database)
-
-    # Respond to the client
-    return jsonify({"message": "Rectangle received successfully!", "topLeft": top_left, "bottomRight": bottom_right})
 
 
 
