f61e89bbe9d55f29b5ea58a1ffeb59b76b66fd9f
s30274 <s30274@pjwstk.edu.pl>
2024-11-23 13:02:01 +0100

    Changed from camera input to file input

--- vision/main.py
+++ vision/main.py
@@ -1,30 +1,36 @@
+from time import sleep
+
 import cv2
 
 # Open the default camera
-cam = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('input.mp4')
 
 # Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
 while True:
-    ret, frame = cam.read()
+    ret, frame = cap.read()
 
-    # Write the frame to the output file
-    out.write(frame)
+    if(ret==True):
+        # Write the frame to the output file
+        out.write(frame)
 
-    # Display the captured frame
-    cv2.imshow('Camera', frame)
+        # Display the captured frame
+        cv2.imshow('Camera', frame)
 
     # Press 'q' to exit the loop
-    if cv2.waitKey(1) == 27:
+    if ret==False or cv2.waitKey(1) == 27:
         break
 
+    # display frame for 1/30 of a second (30fps)
+    sleep(1/30)
+
 # Release the capture and writer objects
-cam.release()
+cap.release()
 out.release()
 cv2.destroyAllWindows()
