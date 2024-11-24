b7469a853b2a9b0609f08b8eea1a2cc0a702219e
Sewimi <dawidf0103@gmail.com>
2024-11-24 07:18:34 -0500

    last commit i think

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -84,4 +84,20 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(points);
     connectionManager.emit("send_drones", rectangleBounds || points);
   });
+
+
+
+  const fs = require('fs')
+  const filePath = './record.txt'
+  fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+          console.error('Error reading file:', err);
+          return;
+      }
+      console.log('File content:', data);
 });
+
+
+
+});
+
